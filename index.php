ce4758d0cefb4e85762ea6346e4ab75d1a9726f6
Georgy Sharapov <georgewebp@gmail.com>
2020-03-28 22:44:45 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: george
+ * Date: 28/03/2020
+ * Time: 5:06:51 PM
+ */
+
+// connect to database
+require_once('db/db.php');
+
+// connect html main file
+require_once('view/index.html.php');
